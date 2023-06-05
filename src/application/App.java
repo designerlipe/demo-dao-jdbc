bc5a7b37755f1e1b428ae2808e70a7985c3167f7
Felipe Lacerda <designerlipe@gmail.com>
2023-06-05 19:45:58 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/App.java
+++ src/application/App.java
@@ -10,6 +10,6 @@ public class App {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-        System.out.println(seller);
+        System.out.println(seller); 
     }
 }
