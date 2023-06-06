a3f42a497a1d425d498415edf1af439b51aa8c55
Felipe Lacerda <designerlipe@gmail.com>
2023-06-05 22:31:43 -0300

    findById implementation

--- src/application/App.java
+++ src/application/App.java
@@ -1,19 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model_dao.DaoFactory;
 import model_dao.SellerDao;
-import model_entities.Department;
 import model_entities.Seller;
 
 public class App {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        System.out.println(seller); 
+
+        Seller seller = sellerDao.findById(3);
+        
+        System.out.println(seller);
     }
 }
--- src/model_dao_impl/SellerDaoJDBC.java
+++ src/model_dao_impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model_dao_impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model_dao.SellerDao;
+import model_entities.Department;
 import model_entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
 
     @Override
     public void insert(Seller obj) {
@@ -27,8 +40,37 @@ public class SellerDaoJDBC implements SellerDao{
 
     @Override
     public Seller findById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "From seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.id "
+                            + "WHERE seller.Id = ?");
+
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if(rs.next()) {
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                obj.setEmail(rs.getString("Email"));
+                obj.setBaseSalary(rs.getDouble("BaseSalary"));
+                obj.setBirthDate(rs.getDate("BirthDate"));
+                obj.setDepartment(dep);
+                return obj;
+            }  
+            return null;              
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
@@ -36,5 +78,5 @@ public class SellerDaoJDBC implements SellerDao{
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findAll'");
     }
-    
+
 }
